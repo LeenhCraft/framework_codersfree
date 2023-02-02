121886f3f5c65c9d69c67be61ce5c4e774902a04
leenh bustmante <hackingleenh@gmail.com>
2023-02-02 00:18:45 -0500

    consideraciones en mayusculas y minusculas en las rutas

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once '../config/database.php';
-require_once '../app/Http/Helpers.php';
+require_once '../App/Http/Helpers.php';
 require_once '../autoload.php';
 require_once '../routes/web.php';
